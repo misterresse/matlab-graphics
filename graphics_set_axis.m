28b2ce4d787938f69e2d12f9adb7a822f13bfd6a
李倍存 <mister.resse@outlook.com>
2016-11-18 10:34:51 +0800

    fix #1

--- graphics_set_axis.m
+++ graphics_set_axis.m
@@ -4,8 +4,10 @@ function graphics_set_axis(xAndYAxisBounds, xAndYAxisDivs)
 
     [xAxisStartValue, xAxisEndValue, yAxisStartValue, yAxisEndValue]    = common_spilt(xAndYAxisBounds);
     [xAxisDivValue ,yAxisDivValue]                                      = common_spilt(xAndYAxisDivs);
+    xlim([xAxisStartValue xAxisEndValue]);
+    ylim([yAxisStartValue yAxisEndValue]);
     set(gca,'XTick',xAxisStartValue:xAxisDivValue:xAxisEndValue);
-    %set(gca,'YTick',yAxisStartValue:yAxisDivValue:yAxisEndValue);
+    set(gca,'YTick',linspace(yAxisStartValue, yAxisEndValue, yAxisDivValue));
 
 
 end
