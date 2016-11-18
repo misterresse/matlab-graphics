57af7281d0b232ce1afcb34f356a65547bbefa62
李倍存 <mister.resse@outlook.com>
2016-11-18 11:31:03 +0800

    fix

--- graphics_set_axis.m
+++ graphics_set_axis.m
@@ -1,13 +1,13 @@
 
 function graphics_set_axis(xAndYAxisBounds, xAndYAxisDivs)
-   
+
 
     [xAxisStartValue, xAxisEndValue, yAxisStartValue, yAxisEndValue]    = common_spilt(xAndYAxisBounds);
     [xAxisDivValue ,yAxisDivValue]                                      = common_spilt(xAndYAxisDivs);
     xlim([xAxisStartValue xAxisEndValue]);
     ylim([yAxisStartValue yAxisEndValue]);
     set(gca,'XTick',xAxisStartValue:xAxisDivValue:xAxisEndValue);
-    set(gca,'YTick',linspace(yAxisStartValue, yAxisEndValue, yAxisDivValue));
+    set(gca,'YTick',linspace(yAxisStartValue, yAxisEndValue, yAxisDivValue+1));
 
 
 end
